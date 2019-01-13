d336cba60508c473683ecc33f2a39a63210301cc
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-01-13 17:52:10 +0400

    Supported token address

--- ethereum_common.sh
+++ ethereum_common.sh
@@ -14,7 +14,7 @@ roslaunch ethereum_common erc20.launch \
     ens_contract:="0x4e978ed8A05b516D8130Ff7dF54Fbc8b7ceB6442" \
     lighthouse_contract:="airalab.lighthouse.4.robonomics.sid" \
     factory_contract:="factory.4.robonomics.sid" \
-    erc20_token:="xrt.4.robonomics.sid" \
+    erc20_token:="0x093ac06910f23570292fd5027a4fA558ed4Cd010" \
     keyfile:="$WORKSPACE/keyfile" \
     keyfile_password_file:="$WORKSPACE/keyfile_password_file" \
     web3_http_provider:="https://sidechain.aira.life/rpc" \
